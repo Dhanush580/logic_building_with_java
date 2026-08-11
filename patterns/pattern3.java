0e7a44aac83ecbf6cc9f9efa92ade4f472caceb8
Dhanush Pekala <119430623+Dhanush580@users.noreply.github.com>
2026-08-11 17:07:29 +0530

    Create pattern3.java

---
+++ patterns/pattern3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    //pattern3
+	    
+	    Scanner ip=new Scanner(System.in);
+	    int n=ip.nextInt();
+	    for(int i=0; i<n; i++){
+	        for(int j=0; j<i; j++){
+	            System.out.print(" ");
+	        }
+	        for(int j=0; j<n; j++){
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+
+      // ip: 5
+    
+      // op:
+    
+      // *****
+      //  *****
+      //   *****
+      //    *****
+      //     *****
+
+	}
+}
